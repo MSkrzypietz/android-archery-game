bed49dd129f35a3c785c5dca68a56498026ac55e
Michael Skrzypietz <michael.s97@web.de>
2017-08-24 20:10:56 +0200

    refactored LoadGameActivity

--- app/src/main/java/com/example/michael/archerygame/LoadGameActivity.java
+++ app/src/main/java/com/example/michael/archerygame/LoadGameActivity.java
@@ -2,22 +2,19 @@ package com.example.michael.archerygame;
 
 import android.content.Intent;
 import android.database.Cursor;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import java.util.ArrayList;
 import com.example.michael.archerygame.data.GameContract.GameEntry;
 
-import static com.example.michael.archerygame.GameActivity.gameId;
+import java.util.ArrayList;
 
 public class LoadGameActivity extends AppCompatActivity {
 
-    private ArrayAdapter<String> itemsAdapter;
     private ArrayList<Long> gameIdList = new ArrayList<>();
 
     @Override
@@ -25,7 +22,7 @@ public class LoadGameActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_load_game);
 
-        itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getGameDates());
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, getGameDates());
         ListView listView = (ListView) findViewById(R.id.gameListView);
         listView.setAdapter(itemsAdapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -45,20 +42,16 @@ public class LoadGameActivity extends AppCompatActivity {
                 GameEntry.COLUMN_GAME_DATE_NR
         };
 
-        Cursor cursor = getContentResolver().query(GameEntry.CONTENT_URI, projection, null, null, GameEntry.COLUMN_GAME_DATE + " DESC, " +GameEntry.COLUMN_GAME_DATE_NR + " DESC");
-
         ArrayList<String> gameList = new ArrayList<>();
 
-        try {
-            while (cursor.moveToNext()) {
+        try (Cursor cursor = getContentResolver().query(GameEntry.CONTENT_URI, projection, null, null, GameEntry.COLUMN_GAME_DATE + " DESC, " +GameEntry.COLUMN_GAME_DATE_NR + " DESC")) {
+            while (cursor != null && cursor.moveToNext()) {
                 String gameDate = cursor.getString(cursor.getColumnIndex(GameEntry.COLUMN_GAME_DATE));
                 String gameDateNr = cursor.getString(cursor.getColumnIndex(GameEntry.COLUMN_GAME_DATE_NR));
                 Long gameId = cursor.getLong(cursor.getColumnIndex(GameEntry._ID));
                 gameList.add(gameDate + ": " + gameDateNr);
                 gameIdList.add(gameId);
             }
-        } finally {
-            cursor.close();
         }
         return gameList;
     }
